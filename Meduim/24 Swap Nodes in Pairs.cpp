27214627323fd9379c15110dc04af3b3a5d2ff30
yuyue19 <1063219646@qq.com>
2021-03-14 12:54:34 +0800

    20210314

--- Meduim/24 Swap Nodes in Pairs.cpp
+++ Meduim/24 Swap Nodes in Pairs.cpp
@@ -26,12 +26,16 @@ public:
 
         // 每次需要交换temp后面的两个节点
         while (temp->next != nullptr && temp->next->next != nullptr) {
+            // 获取需要更新的两个节点
             ListNode *node1 = temp->next;
             ListNode *node2 = temp->next->next;
 
-            temp->next = node2;
-            node1->next = node2->next;
-            node2->next = node1;
+            // 更新节点的指针关系实现两两交换
+            temp->next = node2; // node2接到temp后
+            node1->next = node2->next; // 断开的节点连到后续
+            node2->next = node1; // node1变成node2的后继
+
+            // 对链表中其余两两节点进行交换
             temp = node1;
         }
 
