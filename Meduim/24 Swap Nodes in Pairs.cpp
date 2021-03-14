74d6c5e717c882c61bd6db5a7ddeefad07e8d668
yuyue19 <1063219646@qq.com>
2021-03-14 12:29:20 +0800

    20210314

--- Meduim/24 Swap Nodes in Pairs.cpp
+++ Meduim/24 Swap Nodes in Pairs.cpp
@@ -17,9 +17,23 @@ public:
         head->next = swapPairs(newHead->next);
         newHead->next = head;
 
-        return newHead;
+        //return newHead;
 
         // 迭代版本
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *temp = dummy;
 
+        while (temp->next != nullptr && temp->next->next != nullptr) {
+            ListNode *node1 = temp->next;
+            ListNode *node2 = temp->next->next;
+
+            temp->next = node2;
+            node1->next = node2->next;
+            node2->next = node1;
+            temp = node1;
+        }
+
+        return dummy->next;
     }
 };
