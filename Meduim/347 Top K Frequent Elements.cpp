268d2bde4ffbbf1757e751967500b45e3143ed95
yuyue19 <1063219646@qq.com>
2021-04-02 16:09:03 +0800

    20210402

--- Meduim/347 Top K Frequent Elements.cpp
+++ Meduim/347 Top K Frequent Elements.cpp
@@ -4,15 +4,40 @@
 
 # include <vector>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 
 class Solution_347 {
 public:
+    static bool compare(pair<int, int> &m, pair<int, int> &n) {
+        return m.second > n.second;
+    }
+
     vector<int> topKFrequent(vector<int> &nums, int k) {
         unordered_map<int, int> map;
         for (auto key:nums) {
             map[key]++;
         }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(&compare)> q(compare);
+        for (auto &[num, count]:map) {
+            if (q.size() == k) {
+                if (q.top().second < count) {
+                    q.pop();
+                    q.emplace(num, count);
+                }
+            } else {
+                q.emplace(num, count);
+            }
+        }
+
+        vector<int> ans;
+        while (!q.empty()) {
+            ans.emplace_back(q.top().first);
+            q.pop();
+        }
+
+        return ans;
     }
 };
