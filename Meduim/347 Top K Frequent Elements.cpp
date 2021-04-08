426f33e7c732b0ff38892bae309bae668413cc68
yuyue19 <1063219646@qq.com>
2021-04-08 22:40:22 +0800

    20210408

--- Meduim/347 Top K Frequent Elements.cpp
+++ Meduim/347 Top K Frequent Elements.cpp
@@ -26,6 +26,8 @@ public:
         // 按次序取映射中的元素，放入堆中比较
         for (auto &[num, count]:map) {
             if (q.size() == k) {
+                // 堆顶小于当前数字出现的次数，由于是小顶堆，说明存在某个数字次数比当前数字次数小
+                // 弹出堆订顶，将当前值插入堆中
                 if (q.top().second < count) {
                     q.pop();
                     q.emplace(num, count);
---
+++ Meduim/560 Subarray Sum Equals K.cpp
@@ -0,0 +1,25 @@
+//
+// Created by YuYue on 2021/4/2.
+//
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution_560 {
+public:
+    int subarraySum(std::vector<int> &nums, int k) {
+        unordered_map<int, int> map;
+        map[0] = 1;
+        int count = 0, pre = 0;
+
+        for (auto x:nums) {
+            pre += x;
+            if (map.find(pre - k) != map.end()) {
+                count += map[pre - k];
+            }
+            map[pre]++;
+        }
+        return count;
+    }
+};
